8f647581aa8b62f3770fde3d71ffb4b5520e4cc6
mavli1999 <134156002+mavli1999@users.noreply.github.com>
2024-10-31 22:31:54 -0400

    Update twttr.py

--- twttr.py
+++ twttr.py
@@ -1,16 +1,23 @@
-text=input("Input: ")
 vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 
-for vowel in vowels:
-    #In each iteration, vowel is assigned to the element from the vowels list
+def shorten(word):
+    for vowel in vowels:
+        word = word.replace(vowel, "") #remove vowels
 
+    return word
+
+    #In each iteration, vowel is assigned to the element from the vowels list
     # In the first iteration, vowel = 'a'
     # In the second iteration, vowel = 'e':
     # In the third iteration, vowel = 'i':
     # In the fourth iteration, vowel = 'o'
     # In the fifth iteration, vowel = 'u'.
-
-    text = text.replace(vowel, "")
     #For each vowel, replace() is called, replacing all occurrences of that vowel
 
-print(text)
+def main():
+    word=input("Input: ")
+    text=shorten(word)
+    print(text)
+
+if __name__ == "__main__":
+    main()
