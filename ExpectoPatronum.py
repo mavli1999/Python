be1eb7273290b3d5ab49d44c0f64fd3b296f9de3
“Mav <lishicong1999@email.comgit config --global user.name John>
2024-11-17 18:04:40 -0500

    Added

---
+++ ExpectoPatronum.py
@@ -0,0 +1,43 @@
+class Student:
+    def __init__(self, name, house, patronus):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        if patronus and patronus not in ["Stag", "Otter", "Jack Russell terrier"]:
+            raise ValueError("Invalid patronus")
+        self.name = name
+        self.house = house
+        self.patronus = patronus
+
+    def __str__(self): #defines how an object is represented as a string when it is converted to one
+        return f"{self.name} from {self.house}"
+    
+    def charm(self): #where specific cases have specific results
+        match self.patronus: #if patronus exists
+            case "Stag":
+                return "🐴"
+            case "Otter":
+                return "🦦"
+            case "Jack Russell terrier":
+                return "🐶"
+            case _:
+                return "🪄"
+
+
+def main():
+    student = get_student() #student = the class from get_student()
+    print(student) #display the class 
+    #When print(student) is called, Python implicitly calls the __str__ method of the Student class
+    print("Expecto Patronum!", student.charm())
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    patronus = input("Patronus: ") or None
+    return Student(name, house, patronus) #the class is being created and returned
+
+
+if __name__ == "__main__":
+    main()
